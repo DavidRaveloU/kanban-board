9de073d9d60b2f4c23593a7cabd185e8a825907f
DavidRaveloU <dravelo@unicesar.edu.co>
2025-11-28 18:13:14 -0500

    Refactor Kanban import to use dynamic loading for improved performance

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
-import Kanban from "./components/Kanban";
+"use client";
 
+import dynamic from "next/dynamic";
+
+const Kanban = dynamic(() => import("./components/Kanban"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="flex flex-col items-start mt-4">
-      <h1 className="text-4xl font-bold">Kanban Board</h1>
+        <h1 className="text-4xl font-bold">Kanban Board</h1>
         <span className="text-muted-foreground">By David Ravelo</span>
-        <span className="text-muted-foreground">Created with Next.js, Radix UI and TailwindCSS</span>
+        <span className="text-muted-foreground">
+          Created with Next.js, Radix UI and TailwindCSS
+        </span>
       </div>
-      <Kanban/>
+
+      <Kanban />
     </main>
   );
 }
